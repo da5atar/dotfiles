da36ab4b5a8ddb08d34837c9d588822089e21e67
Massamba Sow <mass.sow@gmail.com>
2026-03-23 09:41:20 +0000

    feat(dotfiles): Reorganize dotfiles - Add NeoVim
    Adds:
        - ZSH config files (.zshenv, .zprofile -->
        .profile and .zlogout)
        - Functions and aliases for NeoVim
        - Live Grep task to Zed
        - Pyenv
        - Lunarvim
        - LazyVim
        - Dependencies for neovim
        - Yazi integration
        - Navi integration
        - TabbyML config
        - default configs for Atuin and Television

---
+++ dotfiles/.config/nvim_lazyvim/lua/plugins/text-case.lua
@@ -0,0 +1,12 @@
+-- text-case.lua
+-- provides a set of commands to quickly change the case of text
+-- --
+return {
+  "johmsalas/text-case.nvim",
+  lazy = false,
+  config = true,
+  cmd = {
+    "Subs",
+    "TextCaseStartReplacingCommand",
+  },
+}
