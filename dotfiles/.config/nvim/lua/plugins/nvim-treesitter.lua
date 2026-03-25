8cc4177518f24e3c594638426592f2653da1ea97
Massamba Sow <mass.sow@gmail.com>
2026-03-25 13:44:46 +0000

    chore(nvim): Configure nvim-treesitter-text-objects
    Add default configuration options
    Also for future reference/modifications

--- dotfiles/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -47,63 +47,116 @@ return {
     branch = 'main',
     event = 'BufReadPost',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+
+      -- Or, disable per filetype (add as you like)
+      -- vim.g.no_python_maps = true
+      -- vim.g.no_ruby_maps = true
+      -- vim.g.no_rust_maps = true
+      -- vim.g.no_go_maps = true
+    end,
     config = function()
       require('nvim-treesitter-textobjects').setup({
         select = {
+          -- Automatically jump forward to textobj, similar to targets.vim
           lookahead = true,
-        },
-        move = {
-          set_jumps = true,
-        },
+          -- You can choose the select mode (default is charwise 'v')
+          --
+          -- Can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * method: eg 'v' or 'o'
+          -- and should return the mode ('v', 'V', or '<c-v>') or a table
+          -- mapping query_strings to modes.
+          selection_modes = {
+            -- ['@parameter.outer'] = 'v', -- charwise
+            -- ['@function.outer'] = 'V',  -- linewise
+            -- ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          move = {
+            -- whether to set jumps in the jumplist
+            -- This is similar to ]m, [m, ]M, [M Neovim's mappings to jump to the next or previous function.
+            set_jumps = true,
+          },
+        }
       })
       local map = vim.keymap.set
 
       -- Textobject selections
       map({ 'x', 'o' }, 'af', function()
-        require('nvim-treesitter-textobjects.select').select_textobject('@function.outer', 'textobjects')
-      end, { desc = 'outer function' })
+        require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", 'textobjects')
+      end, { desc = "outer function" })
       map({ 'x', 'o' }, 'if', function()
-        require('nvim-treesitter-textobjects.select').select_textobject('@function.inner', 'textobjects')
-      end, { desc = 'inner function' })
+        require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", 'textobjects')
+      end, { desc = "inner function" })
       map({ 'x', 'o' }, 'ac', function()
-        require('nvim-treesitter-textobjects.select').select_textobject('@class.outer', 'textobjects')
-      end, { desc = 'outer class' })
+        require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", 'textobjects')
+      end, { desc = "outer class" })
       map({ 'x', 'o' }, 'ic', function()
-        require('nvim-treesitter-textobjects.select').select_textobject('@class.inner', 'textobjects')
-      end, { desc = 'inner class' })
+        require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", 'textobjects')
+      end, { desc = "inner class" })
       map({ 'x', 'o' }, 'aa', function()
-        require('nvim-treesitter-textobjects.select').select_textobject('@parameter.outer', 'textobjects')
-      end, { desc = 'outer argument' })
+        require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", 'textobjects')
+      end, { desc = "outer argument" })
       map({ 'x', 'o' }, 'ia', function()
-        require('nvim-treesitter-textobjects.select').select_textobject('@parameter.inner', 'textobjects')
-      end, { desc = 'inner argument' })
+        require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", 'textobjects')
+      end, { desc = "inner argument" })
+      --
 
       -- Movement
       map({ 'n', 'x', 'o' }, ']f', function()
-        require('nvim-treesitter-textobjects.move').goto_next_start('@function.outer', 'textobjects')
-      end, { desc = 'next function start' })
+        require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+      end, { desc = "next function start" })
       map({ 'n', 'x', 'o' }, '[f', function()
-        require('nvim-treesitter-textobjects.move').goto_previous_start('@function.outer', 'textobjects')
-      end, { desc = 'previous function start' })
+        require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+      end, { desc = "previous function start" })
       map({ 'n', 'x', 'o' }, ']F', function()
-        require('nvim-treesitter-textobjects.move').goto_next_end('@function.outer', 'textobjects')
-      end, { desc = 'next function end' })
+        require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
+      end, { desc = "next function end" })
       map({ 'n', 'x', 'o' }, '[F', function()
-        require('nvim-treesitter-textobjects.move').goto_previous_end('@function.outer', 'textobjects')
-      end, { desc = 'previous function end' })
+        require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
+      end, { desc = "previous function end" })
       map({ 'n', 'x', 'o' }, ']k', function()
-        require('nvim-treesitter-textobjects.move').goto_next_start('@class.outer', 'textobjects')
-      end, { desc = 'next class start' })
+        require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+      end, { desc = "next class start" })
       map({ 'n', 'x', 'o' }, '[k', function()
-        require('nvim-treesitter-textobjects.move').goto_previous_start('@class.outer', 'textobjects')
-      end, { desc = 'previous class start' })
+        require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+      end, { desc = "previous class start" })
       map({ 'n', 'x', 'o' }, ']K', function()
-        require('nvim-treesitter-textobjects.move').goto_next_end('@class.outer', 'textobjects')
-      end, { desc = 'next class end' })
+        require("nvim-treesitter-textobjects.move").goto_next_end("@class.outer", "textobjects")
+      end, { desc = "next class end" })
       map({ 'n', 'x', 'o' }, '[K', function()
-        require('nvim-treesitter-textobjects.move').goto_previous_end('@class.outer', 'textobjects')
-      end, { desc = 'previous class end' })
+        require("nvim-treesitter-textobjects.move").goto_previous_end("@class.outer", "textobjects")
+      end, { desc = "previous class end" })
+      --
+
+      -- Swaps
+      map({ 'n', 'x', 'o' }, "<leader>a", function()
+        require("nvim-treesitter-textobjects.swap").swap_textobject("@parameter.inner", "textobjects")
+      end, { desc = "swap argument" })
+
+      -- repeat
+      local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
+
+      -- Repeat movement with ; and ,
+      -- ensure ; goes forward and , goes backward regardless of the last direction
+      map({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next, { desc = "repeat last move next" })
+      map({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous, { desc = "repeat last move previous" })
+
+      -- vim way: ; goes to the direction you were moving.
+      -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move, { desc = "repeat last move" })
+      -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite, { desc = "repeat last move opposite" })
+
+      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+      map({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+      map({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+      map({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+      map({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+      --
     end,
+    --
   },
   -- Context
   {
