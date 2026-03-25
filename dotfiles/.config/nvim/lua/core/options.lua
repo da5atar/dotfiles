6f50e1db28cde8535e4acf4c2bc2ec5ce0899936
Massamba Sow <mass.sow@gmail.com>
2026-03-25 21:56:46 +0000

    feat(nvim): Add dashboard
    Adds:
      - alpha_nvim
        Updates:
      - which-key
      - nvim options

--- dotfiles/.config/nvim/lua/core/options.lua
+++ dotfiles/.config/nvim/lua/core/options.lua
@@ -1,25 +1,56 @@
 -- sets up vim options
+--
+local opt = vim.opt; -- Vim optionals
+--
+-- Hint: use `:h <option>` to figure out the meaning if needed
 
-vim.opt.number = true             -- show line numbers
-vim.opt.relativenumber = true     -- show relative line numbers
+-- Clipboard & Mouse
+opt.clipboard = 'unnamedplus' -- Use system clipboard
+opt.mouse = "a"               -- allow the mouse to be used in Nvim
+--
 
-vim.opt.splitright = true         -- open new splits to the right
-vim.opt.splitbelow = true         -- open new splits below
+opt.completeopt = { "menu", "menuone", "noselect" }
+opt.fixeol = false   -- Turn off appending new line in the end of a file
+opt.swapfile = false -- no .swp file
 
-vim.opt.wrap = false              -- don't wrap lines
+-- Folding
+opt.foldmethod = 'syntax'
 
-vim.opt.expandtab = true          -- use spaces instead of tabs
-vim.opt.tabstop = 2               -- number of spaces per tab
-vim.opt.shiftwidth = 2            -- number of spaces to use for indentation
+-- Indentation
+opt.expandtab = true   -- use spaces instead of tabs
+opt.tabstop = 2        -- number of spaces per tab
+opt.shiftwidth = 2     -- number of spaces to use for indentation
+opt.smartindent = true -- Turn on smart indentation. See in the docs for more info
+--
 
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
+-- Searching
+opt.ignorecase = true    -- Ignore case if all characters in lower case
+opt.joinspaces = false   -- Join multiple spaces in search
+opt.smartcase = true     -- When there is a one capital letter search for exact match
+opt.showmatch = true     -- Highlight search instances
+opt.incsearch = true     -- search as characters are entered
+opt.inccommand = "split" -- show incremental search results in a split
+-- opt.hlsearch = false  -- do not highlight matches
+--
 
-vim.opt.scrolloff = 999           -- number of lines to keep above/below cursor
+-- UI config
+opt.number = true         -- show line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.cursorline = true     -- highlight cursor line underneath the cursor horizontally
+opt.termguicolors = true  -- enable true color support
+-- Buffer
+opt.scrolloff = 999       -- number of lines to keep above/below cursor
+opt.virtualedit = "block" -- allow virtual editing in block mode
+opt.wrap = false          -- don't wrap lines
+-- Window
+opt.splitbelow = true     -- Put new windows below current
+opt.splitright = true     -- Put new vertical splits to right
+opt.winborder = "rounded" -- add rounded border for floating window
+--
 
-vim.opt.virtualedit = "block"     -- allow virtual editing in block mode
+-- Wild Menu
+opt.wildmenu = true
+opt.wildmode = "longest:full,full"
+--
 
-vim.opt.inccommand = "split"      -- show incremental search results in a split
-
-vim.opt.ignorecase = true         -- ignore case in search
-
-vim.opt.termguicolors = true      -- enable true color support
+-- vim: tabstop=2 shiftwidth=2 expandtab syntax=lua foldmethod=marker foldlevelstart=1
---
+++ dotfiles/.config/nvim/lua/plugins/alpha_nvim.lua
@@ -0,0 +1,80 @@
+-- credits: https://github.com/Baguma03
+return {
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons", "ibhagwan/fzf-lua" },
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      -- Create a custom footer/message section
+      local message = {
+        type = "text",
+        val = "What we assembled with trembling hands, rose to question its maker.",
+        opts = {
+          hl = "Comment", -- Using a subtle color; change to "Statement" for more pop
+          position = "center",
+        },
+      }
+
+      -- Helper function to convert image to ASCII
+      local function get_image_header(path)
+        local cmd = "ascii-image-converter " .. path .. " -b --width 180"
+
+        local handle = io.popen(cmd)
+        local result = handle:read("*a")
+        handle:close()
+
+        local lines = {}
+        for line in result:gmatch("([^\n]*)\n?") do
+          table.insert(lines, line)
+        end
+        return lines
+      end
+
+      -- Path to image
+      local image_path = os.getenv("HOME") .. "/Pictures/adam-and-machine.png" -- set width to 50
+
+      -- Set the header to the output of our function
+      dashboard.section.header.val = get_image_header(image_path)
+
+      -- Apply a colorscheme highlight group
+      dashboard.section.header.opts.hl = "Statement"
+      dashboard.section.buttons.opts.hl = "Number"
+
+      -- Dashboard Buttons using fzf-lua
+      dashboard.section.buttons.val = {
+        dashboard.button("f", "󰈞  Find Files", ":FzfLua files<CR>", { desc = "Find Files" }),
+        dashboard.button("r", "  Recent Files", ":FzfLua oldfiles<CR>", { desc = "Recent Files" }),
+        dashboard.button("p", "󰚰  Command History", ":FzfLua command_history<CR>", { desc = "Command History" }),
+        dashboard.button("C", "  Config", ":lua require('fzf-lua').files({cwd=vim.fn.stdpath('config')})<CR>",
+          { desc = "Config" }),
+        dashboard.button("U", "󰄉  Update", ":Lazy update<CR>", { desc = "Update" }),
+        dashboard.button("q", "󰅚  Quit", ":qa<CR>", { desc = "Quit" }),
+      }
+
+      -- Assemble the layout
+      -- Insert the message between the header and the buttons
+      dashboard.config.layout = {
+        { type = "padding", val = 2 },
+        dashboard.section.header,
+        { type = "padding", val = 2 },
+        message, -- Your new text message
+        { type = "padding", val = 2 },
+        dashboard.section.buttons,
+        dashboard.section.footer,
+      }
+
+      -- Hiding the tildes
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "AlphaReady",
+        callback = function()
+          vim.opt_local.fillchars = { eob = " " }
+        end,
+      })
+
+      alpha.setup(dashboard.opts)
+    end,
+  },
+}
